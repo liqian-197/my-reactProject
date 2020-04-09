58ea527db1421fdafa000598b5f8b2af21facc16
李倩 <58423209+liqian-197@users.noreply.github.com>
2020-04-09 20:46:22 +0800

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,27 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import ReactDom from 'react-dom';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+
+//引入全局样式，字体控制js
+import './utils/font';
+import './assets/css/index.css'
+
+import Default from "./layouts/default";
+
+import {BrowserRouter as Router, Route} from 'react-router-dom';
+
+import './plugins/axios'
+
+//在react应用内部创建全局属性
+import {serverBaseUrl} from './server';
+React.baseUrl = serverBaseUrl;
+React.Component.prototype.baseUrl=serverBaseUrl;
+
+ReactDom.render(
+  <Router>
+    <Route component={Default}/>
+  </Router>
+  ,
+  document.querySelector('#root'),
+  ()=>console.log('react-dom render')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
